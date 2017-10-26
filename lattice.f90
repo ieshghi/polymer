4172f47a0d86a4c5a58d91aa62ada58a93ab236d
Iraj Eshghi <irajeshghi@gmail.com>
2017-10-26 15:29:54 -0400

    ...

--- lattice.f90
+++ lattice.f90
@@ -5,12 +5,10 @@ contains
 function genpoly(n)
     implicit none
     integer::n,c,i
-    integer, dimension(:),allocatable::seed
+    integer::seed
     integer, dimension(n)::genpoly ! 0 = north, 1 = east, 2 = south, 3 = west
-    real *8,dimension(n)::r
-    call system_clock(count = c)
+    real *8,dimension()::r
     call random_seed(size = n)
-    allocate(seed(n),source=c+37*[(i,i=0,n-1)])
     call random_seed(put=seed)
     deallocate(seed)
 
