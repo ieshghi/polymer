b1f7c2a003f69222fb9364d4aa5ce733c332cdab
ieshghi <irajeshghi@gmail.com>
2018-10-11 15:48:42 -0400

    done, gets right scalings

--- polymer.py
+++ polymer.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 def genpoly(n,d):
     poly = np.zeros((n,d))
@@ -6,32 +7,49 @@ def genpoly(n,d):
     for i in range(d):
         nbours[i,i] = 1
         nbours[i+d,i] = -1
-    keep = np.zeros((1,d))
     for i in range(1,n):
+        keep = np.zeros((1,d))
         x = poly[i-1,:]
         poss = nbours + x
         for j in range(2*d):
             if(checkocc(poly,i,poss[j,:])==True):
-                keep = vstack((keep,poss[j,:]))
+                keep = np.vstack((keep,poss[j,:]))
         keep = keep[1:,:]        
-        pick = np.random.choice(range(keep.shape(1))) #the only random step in the process
+        if keep.size == 0:
+            return np.nan
+            break
+        pick = np.random.choice(range(keep.shape[0])) #the only random step in the process
         pickpos = keep[pick,:]
         poly[i,:] = pickpos
-    return poly    
+    return poly 
 
+            
 def checkocc(poly,i,test):
     d = poly.shape[1]
-    cons = poly[:i,:]
+    cons = poly[:i-1,:]
     truth = (sum(cons==test,1)==d)
-    if sum(truth)==0:
+    if np.size(truth)==1:
         return True
     else:
-        return False
+        if sum(truth)==0:
+            return True
+        else:
+            return False
 
-def calcr(n,d,nsamp):
-    rs = zeros((nsamp))
+def calcr(nin,d,nsamp):
+    n = int(np.ceil(nin))
+    rs = np.zeros((nsamp))
     for i in range(nsamp):
-        poly = genpoly(n,d)
+        poly = np.nan
+        while np.isnan(np.sum(poly)):
+            poly = genpoly(n,d)
+
         r = np.sqrt(sum((poly[0,:]-poly[-1,:])**2))
         rs[i] = r
     return np.mean(rs),np.std(rs)
+
+def plotpoly(poly):
+    if poly.shape[1]!=2:
+        return 1
+    else:
+       plt.plot(poly[:,0],poly[:,1])
