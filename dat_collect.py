6c347fdf35e8c6f63facd6be373196140582fe08
Iraj Eshghi <ie355@nyu.edu>
2020-05-01 17:06:17 -0400

    fixed typo

--- dat_collect.py
+++ dat_collect.py
@@ -1,13 +1,13 @@
 import sys
 from numpy import *
-n_each = sys.argv[1]
+n_each = int(sys.argv[1])
 ex = loadtxt("datfiles/ideal1.txt")
 
 #first, collect ideals
 r_id = zeros((n_each,ex.size))
 
 for i in range(n_each):
-    r_id[i,:] = loadtxt("datfiles/ideal"+str(i)+".txt")
+    r_id[i,:] = loadtxt("datfiles/ideal"+str(i+1)+".txt")
 
 savetxt("datfiles/ideals.txt",r_id)
 
@@ -15,7 +15,7 @@ savetxt("datfiles/ideals.txt",r_id)
 r_swol = zeros((n_each,ex.size))
 
 for i in range(n_each):
-    r_swol[i,:] = loadtxt("datfiles/swol"+str(i)+".txt")
+    r_swol[i,:] = loadtxt("datfiles/swol"+str(i+1)+".txt")
 
 savetxt("datfiles/swols.txt",r_id)
 
@@ -23,6 +23,6 @@ savetxt("datfiles/swols.txt",r_id)
 r_coll = zeros((n_each,ex.size))
 
 for i in range(n_each):
-    r_co..[i,:] = loadtxt("datfiles/coll"+str(i)+".txt")
+    r_coll[i,:] = loadtxt("datfiles/coll"+str(i+1)+".txt")
 
 savetxt("datfiles/colls.txt",r_coll)
