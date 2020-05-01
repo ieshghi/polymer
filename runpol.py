3c93368c3cfb2f6b385764f5dd3bda8ca8a202cf
Iraj Eshghi <ieshghi@github.com>
2020-05-01 11:43:41 -0400

    fixed small issues

--- runpol.py
+++ runpol.py
@@ -1,8 +1,9 @@
 import simpol as sp
+from numpy import *
 import sys
 
-typ = sys.argv[1]
-n = sys.argv[2]
+typ = int(sys.argv[1])
+n = int(sys.argv[2])
 
 l = 1
 k = 10
@@ -13,17 +14,17 @@ lam_swol = 0
 lam_coll = 10
 
 nsamp = 1
-nstep = 10**5
+nstep = 10
 pollength = 30
 
 if typ==0: #ideal chain
-    mr_id,sr_id,rv_id = getscal(nsamp,pollength,l,k,g,m,T,0,0.01,nstep,1)
+    mr_id,sr_id,rv_id = sp.getscal(nsamp,pollength,l,k,g,m,T,0,0.01,nstep,1)
     savetxt("datfiles/ideal"+str(n)+".txt",rv_id)
 
 elif typ==1: #swollen chain
-    mr_swol,sr_swol,rv_swol = getscal(nsamp,pollength,l,k,g,m,T,lam_swol,0.01,nstep,0)
+    mr_swol,sr_swol,rv_swol = sp.getscal(nsamp,pollength,l,k,g,m,T,lam_swol,0.01,nstep,0)
     savetxt("datfiles/swol"+str(n)+".txt",rv_swol)
 
 elif typ==2: #collapsed chain
-    mr_coll,sr_coll,rv_coll = getscal(nsamp,pollength,l,k,g,m,T,lam_coll,0.01,nstep,0)
+    mr_coll,sr_coll,rv_coll = sp.getscal(nsamp,pollength,l,k,g,m,T,lam_coll,0.01,nstep,0)
     savetxt("datfiles/coll"+str(n)+".txt",rv_swol)
