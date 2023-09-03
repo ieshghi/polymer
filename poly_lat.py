5e082bd3a110f82e9b23bf9f0ed213559ae9d99e
Iraj Eshghi <irajeshghi@gmail.com>
2023-09-02 22:16:12 -0400

    not done switching to new frameworks

--- poly_lat.py
+++ poly_lat.py
@@ -9,7 +9,7 @@ import torch.nn as nn
 print(f'TORCH VERSION: {torch.__version__}')
 import packaging.version
 if packaging.version.parse(torch.__version__) < packaging.version.parse('1.5.0'):
-	raise RuntimeError('Torch versions lower than 1.5.0 not supported')
+    raise RuntimeError('Torch versions lower than 1.5.0 not supported')
 
 #%matplotlib inline
 import matplotlib.pyplot as plt
@@ -17,91 +17,96 @@ import seaborn as sns
 import normflows as nf
 from tqdm import tqdm
 import itertools
+import multiprocessing as mp
+num_c = mp.cpu_count()
+pool = mp.Pool(processes = num_c)
 
 sns.set_style('whitegrid')
 
 if torch.cuda.is_available():
-	torch_device = 'cuda'
-	float_dtype = np.float32 # single
-	torch.set_default_tensor_type(torch.cuda.FloatTensor)
+    torch_device = 'cuda'
+    float_dtype = np.float32 # single
+    torch.set_default_tensor_type(torch.cuda.FloatTensor)
 else:
-	torch_device = 'cpu'
-	float_dtype = np.float64 # double
-	torch.set_default_tensor_type(torch.DoubleTensor)
-	print(f"TORCH DEVICE: {torch_device}")
+    torch_device = 'cpu'
+    float_dtype = np.float64 # double
+    torch.set_default_tensor_type(torch.DoubleTensor)
+    print(f"TORCH DEVICE: {torch_device}")
 
 class energyfunc(nn.Module): #give a penalty = sigma for each overlapping pair of monomers
-	def __init__(self,sigma = 10,lcol = 0.1,bdry=False,constraints = False): #assume step size is unit length, lcol is collision distance
-		super(energyfunc, self).__init__()
-		self.bdry = bdry #if this is set to a finite number, it is the radius of the confining sphere in lattice units
-		self.sigma = sigma #interaction energy for overlapping monomers, in units of K_b T. Probability of overlap should scale like exp(-sigma)
-		self.constraints = constraints #for when we want to eventually include constraints from pore-c or something
+    def __init__(self,sigma = 10,lcol = 0.1,bdry=False,constraints = False): #assume step size is unit length, lcol is collision distance
+        super(energyfunc, self).__init__()
+        self.bdry = bdry #if this is set to a finite number, it is the radius of the confining sphere in lattice units
+        self.sigma = sigma #interaction energy for overlapping monomers, in units of K_b T. Probability of overlap should scale like exp(-sigma)
+        self.constraints = constraints #for when we want to eventually include constraints from pore-c or something
 
-	def forward(self,confdata):
-		energ = 0
+    def forward(self,confdata):
+        energ = 0
         polypos = get_poly(confdata)
-		if self.constraints:
-			energ += check_constraints(conf)
-		if self.bdry:
-			energ += check_bdry(conf)
-		energ += sigma*collisions(polypos,lcol)
-		return energ
+        if self.constraints:
+            energ += check_constraints(conf)
+        if self.bdry:
+            energ += check_bdry(conf)
+        energ += sigma*collisions(polypos,lcol)
+        return energ
 
-	def check_constraints(self,config):
-		return 0
-	def check_bdry(self,config):
-		return 0
+    def check_constraints(self,config):
+        return 0
+    def check_bdry(self,config):
+        return 0
     def collisions(positions,lcol):
         dists = np.array([torch.linalg.norm(x[0]-x[1]) for x in itertools.combinations(positions, 2)])   
         return torch.sum(1/(1+torch.exp(-dists/lcol))) #punish overlap within distance lcol with a sigmoid function
     def get_poly(confdata):
         positions = []
         positions.append(confdata[0:3]) 
-        confdata = confdata[3:]
+        confdata = confdata[3:] + 0.5 #shift the uniform random numbers up so they range [0,1]
         randvals = [i for i in zip(confdata[::2],confdata[1::2])]
-        steps = map(randtovec,randvals)
+        steps = list(map(randtovec,randvals))
+#        pos_arr = np.cumsum(steps,axis=0)
+        return steps
 
 def randtovec(p):
-    ph = (p[0] + 0.5)*2*np.pi
-    th = torch.acos(1-2*(p[1])) #i think the nf.dists module makes uniform distributions on [-0.5,0.5]
+    ph = (p[0])*2*np.pi
+    th = torch.acos(1-2*(p[1])) #nf.dists module makes uniform distributions on [-0.5,0.5]
     x = torch.sin(th)*torch.cos(ph)
     y = torch.sin(th)*torch.sin(ph)
     z = torch.cos(th)
     return torch.Tensor([x,y,z])
 
-N = 100
-dof = 3+2*(N-1) # 3 degrees of freedom for the position of the first monomer, then 2 dof for the 3d orientation of (N-1) unit vectors in an N-step SAW
-prior = nf.distributions.UniformGaussian(dof,range(0,dof),torch.ones(dof))
-
-num_layers = 20
-flow_layers = []
-for i in range(num_layers):
-    # Neural network with two hidden layers having 64 units each
-    # Last layer is initialized by zeros making training more stable
-    param_map = nf.nets.MLP([1, 64, 64, dof], init_zeros=True)
-    # Add flow layer
-    flow_layers.append(nf.flows.AffineCouplingBlock(param_map))
-
-model = nf.NormalizingFlow(prior,flow_layers)
-
-max_iter = 20000
-anneal_iter = 10000
-show_iter = 1000
-loss_hist = np.array([])
-
-optimizer = torch.optim.Adam(model.parameters(), lr=1e-4, weight_decay=1e-6)
-config = prior.sample()
-for it in tqdm(range(max_iter)):
-    optimizer.zero_grad()
-    loss = np.exp(-energyfunc(config))
-    if ~(torch.isnan(loss) | torch.isinf(loss)):
-        loss.backward()
-        optimizer.step()
-    
-    loss_hist = np.append(loss_hist, loss.to('cpu').data.numpy())
-    
-# Plot loss
-plt.figure(figsize=(10, 10))
-plt.plot(loss_hist, label='loss')
-plt.legend()
-plt.savefig('~/public_html/polygen/bla.png') 
+#N = 100
+#dof = 3+2*(N-1) # 3 degrees of freedom for the position of the first monomer, then 2 dof for the 3d orientation of (N-1) unit vectors in an N-step SAW
+#prior = nf.distributions.UniformGaussian(dof,range(dof),torch.ones(dof))[0] #get random numbers uniformly distributed between -1/2 and 1/2
+#
+#num_layers = 20
+#flow_layers = []
+#for i in range(num_layers):
+#    # Neural network with two hidden layers having 64 units each
+#    # Last layer is initialized by zeros making training more stable
+#    param_map = nf.nets.MLP([1, 64, 64, dof], init_zeros=True)
+#    # Add flow layer
+#    flow_layers.append(nf.flows.AffineCouplingBlock(param_map))
+#
+#model = nf.NormalizingFlow(prior,flow_layers)
+#
+#max_iter = 20000
+#anneal_iter = 10000
+#show_iter = 1000
+#loss_hist = np.array([])
+#
+#optimizer = torch.optim.Adam(model.parameters(), lr=1e-4, weight_decay=1e-6)
+#config = prior.sample()
+#for it in tqdm(range(max_iter)):
+#    optimizer.zero_grad()
+#    loss = np.exp(-energyfunc(config))
+#    if ~(torch.isnan(loss) | torch.isinf(loss)):
+#        loss.backward()
+#        optimizer.step()
+#    
+#    loss_hist = np.append(loss_hist, loss.to('cpu').data.numpy())
+#    
+## Plot loss
+#plt.figure(figsize=(10, 10))
+#plt.plot(loss_hist, label='loss')
+#plt.legend()
+#plt.savefig('~/public_html/polygen/bla.png') 
